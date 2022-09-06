2633a4ac5178be980b235168c96bad985c2c9e0e
Hasibul677 <hhasibul677@gmail.com>
2022-09-06 18:21:56 +0600

    not found page create

--- frontend/src/component/layout/NotFound/NotFound.js
+++ frontend/src/component/layout/NotFound/NotFound.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./NotFound.css";
+import {Link} from "react-router-dom"
 
 const NotFound = () => {
-    return (
-        <div>
-            nothing 
+  return (
+    <div className="notfoundParent">
+      <div id="notfound">
+        <div className="notfound">
+          <div className="notfound-404">
+            <h1>404</h1>
+            <h2>Page not found</h2>
+          </div>
+          <Link className="linkpage text-style" to="/home">HOME PAGE</Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
